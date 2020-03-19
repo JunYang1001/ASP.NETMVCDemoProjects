58cf98c1f49b55c0fdaad8a3cefc5e5564d4101a
JunYang1001 <zhangjunyang94@gmail.com>
2020-03-19 21:23:15 +0800

    SimplemeDemo

---
+++ ASP.NETMVCDemoProjects.IdentityDemo/Models/AppIdentityDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ASP.NETMVCDemoProjects.IdentityDemo.Models
+{
+    public class AppIdentityDbContext:IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext():base("IdentityDb")
+        {
+            
+        }
+
+        static AppIdentityDbContext()
+        {
+            System.Data.Entity.Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+
+        public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+        {
+            protected override void Seed(AppIdentityDbContext context)
+            {
+                PerformInitialSetup(context);
+                base.Seed(context);
+            }
+
+            private void PerformInitialSetup(AppIdentityDbContext context)
+            {
+                
+            }
+        }
+    }
+}
---
+++ ASP.NETMVCDemoProjects.SimpleDemo/ViewModel/EmployeeListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NETMVCDemoProjects.SimpleDemo.ViewModel
+{
+    public class EmployeeListViewModel
+    {
+        public List<EmployeeViewModel> Employees { get; set; }
+       public string UserName { get; set; }
+    }
+}
