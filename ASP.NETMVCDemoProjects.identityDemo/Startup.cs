e1a48b33db7a6f971edb2014a160eaf06c6677f8
JunYang1001 <zhangjunyang94@gmail.com>
2020-03-29 10:13:37 +0800

    新增系统identity

---
+++ ASP.NETMVCDemoProjects.identityDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NETMVCDemoProjects.identityDemo.Startup))]
+namespace ASP.NETMVCDemoProjects.identityDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
